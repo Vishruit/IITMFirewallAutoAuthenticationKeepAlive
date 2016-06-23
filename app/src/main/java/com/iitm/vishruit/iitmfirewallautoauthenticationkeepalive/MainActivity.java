83a83e961b01ca14030209d52bb1c43aa958a514
Vishruit <princepaulite@gmail.com>
2016-06-23 18:17:25 +0530

    Added a webView

--- app/src/main/java/com/iitm/vishruit/iitmfirewallautoauthenticationkeepalive/MainActivity.java
+++ app/src/main/java/com/iitm/vishruit/iitmfirewallautoauthenticationkeepalive/MainActivity.java
@@ -1,9 +1,9 @@
 package com.iitm.vishruit.iitmfirewallautoauthenticationkeepalive;
 
 import android.app.Activity;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.WebView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -24,14 +24,20 @@ public class MainActivity extends Activity {
             @Override
             public void onClick(View view) {
 
-                if (et_roll.getText() == null || et_password.getText() == null)
+                if ( et_roll.getText().toString().equals("") || et_password.getText().toString().equals(""))
                     Toast.makeText(MainActivity.this, "Please check your roll number and password.", Toast.LENGTH_SHORT).show();
                 else {
                     setContentView(R.layout.activity_main);
                     TextView tv_hello = (TextView)findViewById(R.id.tv_hello);
                     tv_hello.setText(et_roll.getText().append(et_password.getText()));
+                    mainActivityEnv();
                 }
             }
         });
+    }
+
+    private void mainActivityEnv() {
+        WebView myWebView = (WebView) findViewById(R.id.webView);
+        myWebView.loadUrl("www.yahoo.com");
     }
 }
